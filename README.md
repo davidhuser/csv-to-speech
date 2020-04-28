6357d707d6b1a5c57f9a335bb28aad6b80e12361
David Huser <dhuser@baosystems.com>
2020-04-28 12:25:21 +0200

    convert to requested format

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # csv-to-speech
+
+Uses Google TTS to speak words and converts the .mp3 files to .wav files with sample rate 8KHz, 16 bit, mono, Codec: PCM 16 LE (s16l)
+
+Requirements:
+
+* Python3
+* sox, e.g. ```sudo apt install sox```
+
+Installation:
+
+```bash
+pip install -r requirements.txt
+```
+
+Run:
+
+```bash
+python run.py
+```
--- run.py
+++ run.py
@@ -4,7 +4,10 @@ from csv import DictReader
 
 from gtts import gTTS
 
+from pydub import AudioSegment
+
 LANGUAGES = {'en', 'fr'}
+
 
 def load_csv(path='data.csv', delimiter=','):
     with open(path, 'r') as csvfile:
@@ -12,17 +15,39 @@ def load_csv(path='data.csv', delimiter=','):
         for row in reader:
             yield row
 
+
 def process(row, lang):
-    tts = gTTS(row['text_{}'.format(lang)], lang=lang)
-    path = os.path.join('output', row['file_id_{}'.format(lang)])
+    """text-to-speech to mp3 files in preprocessing folder"""
+    tts = gTTS(row['text_{}'.format(lang)], lang=lang, lang_check=True)
+    file_id = row['file_id_{}'.format(lang)]
+    path = os.path.join('preprocess1', 'pre_' + file_id)
     tts.save(path)
+    return path, file_id
+
+
+def convert(path, file_id):
+    """Convert to wav"""
+    f = AudioSegment.from_mp3(path)
+    f = f.set_channels(1)
+    path = os.path.join('preprocess2', file_id)
+    f.export(path, format='wav')
+    return path, file_id
+
+
+def standardize(path, file_id):
+    """Standardize to voiceXML standard with `sox`"""
+    output = os.path.join('output', file_id)
+    cmd = ' '.join(['sox', path, '--rate 8k', '--bits 16', '--channels 1', '--encoding signed-integer', output])
+    os.system(cmd)
 
 
 def main():
-    for row in load_csv():
-        print(row['text_id'])
+    for i, row in enumerate(load_csv()):
+        print("{} - {}".format(i, row['text_id']))
         for lang in LANGUAGES:
-            process(row, lang)
+            path, file_id = process(row, lang)
+            path, file_id = convert(path, file_id)
+            standardize(path, file_id)
 
 
 if __name__ == '__main__':
